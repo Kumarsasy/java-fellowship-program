67f6f8dbf059b18315270577957ae8e9c5a80364
kumarsasy <kumarsasy@gmail.com>
2022-01-15 16:54:14 +0530

    Day4 assignments

--- src/day4/SnakeAndLadder.java
+++ src/day4/SnakeAndLadder.java
@@ -2,9 +2,46 @@ package day4;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-		
+	static int position = 0;
+	static double dice;
+	static int diceCount=0;
 
+	public static void dice() {
+		dice = Math.floor(Math.random() * 10) % 6 + 1;
+	}
+
+	public static void play() {
+		dice();
+		while(position<=100) {
+			
+		double playDice = Math.floor(Math.random() * 10) % 3;
+		switch ((int) playDice) {
+		case 0:
+			System.out.println("No play"+ dice);
+			position = position;
+			System.out.println(position);
+			diceCount++;
+			break;
+		case 1:
+			System.out.println("Ladder"+ dice);
+			position+=dice;
+			System.out.println(position);
+			diceCount++;
+			break;	
+		case 2:
+			System.out.println("Snake"+ dice);
+			position -=dice;
+			System.out.println(position);
+			diceCount++;
+			break;	
+		}
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		play();
+		System.out.println(diceCount);
 	}
 
 }
